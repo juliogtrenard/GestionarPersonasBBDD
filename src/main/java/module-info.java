692e9949658703440aa60b869636208e1546bf74
Julio <juliogtrenard@gmail.com>
2024-11-05 12:05:46 +0100

    ADD Clase para la conexión.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module es.juliogtrenard.gestionarpersonasBBDD {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens es.juliogtrenard.gestionarpersonasBBDD to javafx.fxml;
