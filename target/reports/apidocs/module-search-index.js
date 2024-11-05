ac50277efad83c490923442de150eeec35f53878
Julio González <juliogtrenard@gmail.com>
2024-11-05 17:54:31 +0100

    FIX gitignore

--- target/reports/apidocs/module-search-index.js
+++
@@ -1,1 +0,0 @@
-moduleSearchIndex = [{"l":"es.juliogtrenard.gestionarpersonasv5"}];updateSearchResults();
--- target/reports/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"es.juliogtrenard.gestionarpersonasv5","l":"es.juliogtrenard.gestionarpersonasv5"},{"m":"es.juliogtrenard.gestionarpersonasv5","l":"es.juliogtrenard.gestionarpersonasv5.controladores"},{"m":"es.juliogtrenard.gestionarpersonasv5","l":"es.juliogtrenard.gestionarpersonasv5.modelos"}];updateSearchResults();
